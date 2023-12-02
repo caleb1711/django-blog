d2683af2b0fe93677af3bb485562d224d6b3474e
caleb1711 <>
2023-12-02 21:08:41 +0000

    deployment keys

--- codestar/settings.py
+++ codestar/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['codestar2023.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['django-experiement-8359e6777191.herokuapp.com', 'localhost']
 
 
 # Application definition
